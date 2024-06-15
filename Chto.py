f6a4d4c8279767ba6f7bff2c63c14343d25741ae
GardenMod <ivandeak814@gmail.com>
2024-06-15 19:34:37 +0300

    Add files via upload

---
+++ Chto.py
@@ -0,0 +1,12 @@
+from . . import loader 
+from telethon.tl.types import Message
+ 
+@loader.tds 
+class photo(loader.Module):
+    """Подпишись на мой канал @userbotik"""
+    strings {"name": "photo"} 
+    async def photocmd(self, message: Message): """Скидывает photo)""" 
+    if message.out: 
+        await message.delete() 
+    avait message.respond(  
+        f*<a href="https://t.me/userbotik/77"</a>. )
