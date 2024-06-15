85677ef720d186a574859cf15499994bfda6a71d
GardenMod <ivandeak814@gmail.com>
2024-06-15 16:30:23 +0300

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -9,4 +9,4 @@ class photo(loader.Module):
     if message.out: 
         await message.delete() 
     avait message.respond(  
-        f*<a href="https://t.me/userbotik/77"‹/a>. )
+        f*<a href="https://t.me/userbotik/77"</a>. )
